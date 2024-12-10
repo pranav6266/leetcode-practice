5c19d50a8946fc5a5a9869b5e52ea5b5a744259c
pranav6266 <pranavchandrashekar5@gmail.com>
2024-12-10 22:38:32 +0530

    Code to return the indices of two numbers in a given array such that they add up to the "target".

--- day1_twoSum.java
+++ day1_twoSum.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class day_1_twoSum {
 
-    public static int[] twoSum(int[] nums, int target) {
+public static int[] twoSum(int[] nums, int target) {
         int[] result=new int[2];
         for(int i=0;i<nums.length-1;i++){
             for(int j=i+1;j<nums.length;j++){
